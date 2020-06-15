ed57fa521052d5386540937ecf8d4601a762f987
Serafim1979 <korolkov.ivan@mail.ru>
2020-06-15 14:56:35 +0400

    Implemented a slider

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -3887,12 +3887,26 @@
 
 $(function() {
 
-    $("#portfolio").slick({
-        arrows: true,
+    var portfolio = $("#portfolio");
+    
+    portfolio.slick({
+        arrows: false,
         dots: true,
         speed: 800,
         slidesToShow: 2,
         slidesToScroll: 2,
+    });
+    
+    $("#portfolio-arrow-prev").on("click", function(event) {
+        event.preventDefault();
+    
+        portfolio.slick('slickPrev');
+    });
+    
+    $("#portfolio-arrow-next").on("click", function(event) {
+        event.preventDefault();
+    
+        portfolio.slick('slickNext');
     });
 
 });
